ead0ad56e091c2d4154bba1d7d47ebb7dc720bc9
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-03-05 01:24:59 +0300

    feat: implemented pieces rendering

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -18,6 +18,34 @@ export const Tile: FC<TileProps> = ({ positionX, positionY }) => {
 
   const hasLabelY: boolean = positionX === 0;
 
+  const isKingW = position === 'e1';
+
+  const isQueenW = position === 'd1';
+
+  const isRookW = ['a1', 'h1'].includes(position);
+
+  const isBishopW = ['c1', 'f1'].includes(position);
+
+  const isKnightW = ['b1', 'g1'].includes(position);
+
+  const isPawnW = ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'].includes(
+    position
+  );
+
+  const isKingB = position === 'e8';
+
+  const isQueenB = position === 'd8';
+
+  const isRookB = ['a8', 'h8'].includes(position);
+
+  const isBishopB = ['c8', 'f8'].includes(position);
+
+  const isKnightB = ['b8', 'g8'].includes(position);
+
+  const isPawnB = ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'].includes(
+    position
+  );
+
   return (
     <div className={tileClassName} data-x={positionX} data-y={positionY}>
       {hasLabelY && (
@@ -28,9 +56,30 @@ export const Tile: FC<TileProps> = ({ positionX, positionY }) => {
         <div className="tile__label tile__label_y">{horizontal[positionX]}</div>
       )}
 
-      {(position === 'a1' || position === 'h1') && (
-        <img className="piece" src="assets/wr.png" alt="wr" />
-      )}
+      {isKingW && <div className="piece piece-king-white"></div>}
+
+      {isQueenW && <div className="piece piece-queen-white"></div>}
+
+      {isRookW && <div className="piece piece-rook-white"></div>}
+
+      {isBishopW && <div className="piece piece-bishop-white"></div>}
+
+      {isKnightW && <div className="piece piece-knight-white"></div>}
+
+      {isPawnW && <div className="piece piece-pawn-white"></div>}
+
+      {isKingB && <div className="piece piece-king-black"></div>}
+
+      {isQueenB && <div className="piece piece-queen-black"></div>}
+
+      {isRookB && <div className="piece piece-rook-black"></div>}
+
+      {isBishopB && <div className="piece piece-bishop-black"></div>}
+
+      {isKnightB && <div className="piece piece-knight-black"></div>}
+
+      {isPawnB && <div className="piece piece-pawn-black"></div>}
+      
     </div>
   );
 };
