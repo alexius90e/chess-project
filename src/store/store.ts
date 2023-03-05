43b08f6604e998eb008512d4dc4218e6bc06f48d
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-03-05 03:14:31 +0300

    feat: added rootstate type at store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { boardReducer } from './board/board.slice';
 
-export default configureStore({
-  reducer: {},
+export const store = configureStore({
+  reducer: {
+    board: boardReducer,
+  },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
