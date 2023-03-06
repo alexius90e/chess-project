cd9d5a09e027b4e49f54bc839742f93fc459e175
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-03-06 13:38:12 +0300

    refactor: refactored boardInitialState

--- src/store/board/board-initial-state.ts
+++ src/store/board/board-initial-state.ts
@@ -2,46 +2,46 @@ import { PieceTeam } from '../../models/piece-team.enum';
 import { PieceType } from '../../models/piece-type.enum';
 import { Piece } from '../../models/piece.interface';
 
-function getPieceTeamById(id: string, teamMark: '1' | '2'): PieceTeam {
-  const regExp = new RegExp(`[a-h][${teamMark}]`, 'g');
+const pieceIds = {
+  whitePawns: ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'],
+  blackPawns: ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'],
+  kings: ['e1', 'e8'],
+  queens: ['d1', 'd8'],
+  rooks: ['a1', 'h1', 'a8', 'h8'],
+  knights: ['b1', 'g1', 'b8', 'g8'],
+  bishops: ['c1', 'f1', 'c8', 'f8'],
+};
+
+const pieceNames = ['king', 'queen', 'rook', 'bishop', 'knight', 'pawn'];
+
+function getPieceTeamById(id: string, type: PieceType): PieceTeam {
+  const isPawn = type === PieceType.Pawn;
+  const regExp = new RegExp(`[a-h][${isPawn ? 2 : 1}]`, 'g');
   return id.match(regExp) ? PieceTeam.White : PieceTeam.Black;
 }
 
-const whitePawnIds = ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'];
+function generatePieces(ids: string[], type: PieceType): Piece[] {
+  const name = pieceNames[type];
+  return ids.map((id: string): Piece => {
+    const team: PieceTeam = getPieceTeamById(id, type);
+    return { id, team, name, type };
+  });
+}
 
-const blacPawnIds = ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'];
+const kings: Piece[] = generatePieces(pieceIds.kings, PieceType.King);
 
-const pawns: Piece[] = [...whitePawnIds, ...blacPawnIds].map(
-  (id: string): Piece => {
-    const team: PieceTeam = getPieceTeamById(id, '2');
-    return { id, team, name: 'pawn', type: PieceType.Pawn };
-  }
+const queens: Piece[] = generatePieces(pieceIds.queens, PieceType.Queen);
+
+const rooks: Piece[] = generatePieces(pieceIds.rooks, PieceType.Rook);
+
+const knights: Piece[] = generatePieces(pieceIds.knights, PieceType.Knight);
+
+const bishops: Piece[] = generatePieces(pieceIds.bishops, PieceType.Bishop);
+
+const pawns: Piece[] = generatePieces(
+  [...pieceIds.whitePawns, ...pieceIds.blackPawns],
+  PieceType.Pawn
 );
-
-const kings: Piece[] = ['e1', 'e8'].map((id: string): Piece => {
-  const team: PieceTeam = getPieceTeamById(id, '1');
-  return { id, team, name: 'king', type: PieceType.King };
-});
-
-const queens: Piece[] = ['d1', 'd8'].map((id: string): Piece => {
-  const team: PieceTeam = getPieceTeamById(id, '1');
-  return { id, team, name: 'queen', type: PieceType.Queen };
-});
-
-const rooks: Piece[] = ['a1', 'h1', 'a8', 'h8'].map((id: string): Piece => {
-  const team: PieceTeam = getPieceTeamById(id, '1');
-  return { id, team, name: 'rook', type: PieceType.Rook };
-});
-
-const knights: Piece[] = ['b1', 'g1', 'b8', 'g8'].map((id: string): Piece => {
-  const team: PieceTeam = getPieceTeamById(id, '1');
-  return { id, team, name: 'knight', type: PieceType.Knight };
-});
-
-const bishops: Piece[] = ['c1', 'f1', 'c8', 'f8'].map((id: string): Piece => {
-  const team: PieceTeam = getPieceTeamById(id, '1');
-  return { id, team, name: 'bishop', type: PieceType.Bishop };
-});
 
 export const boardInitialState: Piece[] = [
   ...kings,
