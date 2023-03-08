a0aa5a5e261f55fdf5e0c18d2d06b1e38639a27c
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-03-09 02:32:12 +0300

    refactor: added types

--- src/components/Rook/Rook.tsx
+++ src/components/Rook/Rook.tsx
@@ -1,5 +1,7 @@
+import { EntityId } from '@reduxjs/toolkit';
 import React, { FC } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { PieceActiveData } from '../../models/piece-active-data.interface';
 import { PieceProps } from '../../models/piece-props.interface';
 import { PieceTeam } from '../../models/piece-team.enum';
 import { boardSelectors } from '../../store/board/board.selectors';
@@ -18,31 +20,26 @@ export const Rook: FC<PieceProps> = ({ piece }) => {
   const dispatch = useDispatch();
 
   function handleClick(): void {
-    const isTheSamePiece = activePiece && activePiece.id === piece.id;
-    const isUnderAttack = activePiece && activePiece.team !== piece.team;
-    const isTeamTurn = currentTeam === piece.team;
+    const isTheSamePiece: boolean = (activePiece && activePiece.id === piece.id) as boolean;
+    const isUnderAttack: boolean = (activePiece && activePiece.team !== piece.team) as boolean;
+    const isTeamTurn: boolean = currentTeam === piece.team;
 
     if (isTheSamePiece) {
       dispatch(boardActions.resetActivePiece());
     } else if (isUnderAttack) {
       dispatch(boardActions.attackByActivePiece(piece));
     } else if (isTeamTurn) {
-      dispatch(
-        boardActions.setActivePiece({
-          piece,
-          moves: checkMoves(),
-          attacks: checkAttacks(),
-        }),
-      );
+      const active: PieceActiveData = { piece, moves: checkMoves(), attacks: checkAttacks() };
+      dispatch(boardActions.setActivePiece(active));
     }
   }
 
   function getRookMoves(): string[][] {
-    const [column, row] = piece.id.split('');
-    const posX = boardAxes.columns.indexOf(column);
-    const posY = boardAxes.rows.indexOf(row);
-    const rookMoveIdsX = boardAxes.columns.map((column) => column + row);
-    const rookMoveIdsY = boardAxes.rows.map((row) => column + row);
+    const [column, row]: string[] = piece.id.split('');
+    const posX: number = boardAxes.columns.indexOf(column);
+    const posY: number = boardAxes.rows.indexOf(row);
+    const rookMoveIdsX: string[] = boardAxes.columns.map((column) => column + row);
+    const rookMoveIdsY: string[] = boardAxes.rows.map((row) => column + row);
     return [
       rookMoveIdsX.slice(0, posX).reverse(),
       rookMoveIdsX.slice(posX + 1),
@@ -52,34 +49,37 @@ export const Rook: FC<PieceProps> = ({ piece }) => {
   }
 
   function checkBlock(id: string, index: number, array: string[]): string {
-    const pieceIds = [...blackPieceIds, ...whitePieceIds];
-    const isBlocked = array.slice(0, index).filter((id) => pieceIds.includes(id)).length > 0;
+    const pieceIds: EntityId[] = [...blackPieceIds, ...whitePieceIds];
+    const isBlocked: boolean =
+      array.slice(0, index).filter((id) => pieceIds.includes(id)).length > 0;
     return isBlocked ? '' : id;
   }
 
   function checkMoves(): string[] {
-    const rookTeamIds = isRookWhite ? whitePieceIds : blackPieceIds;
-    const rookMoves = getRookMoves();
+    const rookTeamIds: EntityId[] = isRookWhite ? whitePieceIds : blackPieceIds;
+    const rookMoves: string[][] = getRookMoves();
     return rookMoves
-      .map((list) =>
+      .map((list: string[]): string[] =>
         list
-          .map((id, index, array) => checkBlock(id, index, array))
-          .filter((id) => !rookTeamIds.includes(id)),
+          .map((id: string, index: number, array: string[]): string => checkBlock(id, index, array))
+          .filter((id: string): boolean => !rookTeamIds.includes(id)),
       )
       .flat();
   }
 
   function checkAttacks(): string[] {
-    const rookMoves = getRookMoves();
-    const rookEnemyIds = isRookWhite ? blackPieceIds : whitePieceIds;
+    const rookMoves: string[][] = getRookMoves();
+    const rookEnemyIds: EntityId[] = isRookWhite ? blackPieceIds : whitePieceIds;
     return rookMoves
       .map(
-        (list) =>
+        (list: string[]): string =>
           list
-            .map((id, index, array) => checkBlock(id, index, array))
-            .filter((id) => rookEnemyIds.includes(id))[0],
+            .map((id: string, index: number, array: string[]): string =>
+              checkBlock(id, index, array),
+            )
+            .filter((id: string): boolean => rookEnemyIds.includes(id))[0],
       )
-      .filter((id) => id);
+      .filter((id: string): boolean => Boolean(id));
   }
 
   return <div className={pieceClassName} onClick={handleClick}></div>;
